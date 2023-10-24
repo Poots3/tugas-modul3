c8410a841a9c1e1bbbd9774b74048cf946d35e77
Al  Ihza <alihza@Als-MacBook-Pro.local>
2023-10-24 14:31:52 +0700

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2023 Simple Website</p>
+      <p>Al Ihza Ahmad</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <div>
+      <h1>Welcome to our Website</h1>
+      <p>Al Ihza Ahmad</p>
+    </div>
+  );
+}
+
+export default Hero;
